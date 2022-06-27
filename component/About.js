8dde15d2edf86a6841efe7189ba1a81d541f039f
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-06-27 18:45:10 +0530

    change about

--- component/About.js
+++ component/About.js
@@ -5,16 +5,26 @@ function About() {
     <div className={styles.about}>
       <h3>About</h3>
       <p>
-        My name is Abhay Pratap Samrat. I am 3rd year computer science student
+        My name is Abhay Pratap Samrat. I am final year computer science student
         attending Kashi Institute Of Technology, Varanasi. <br />
         <br />
         I love working in web development and i want to become a skilled web
-        developer. i like to learn new technologies.
-        <br />
-        <br /> I am interested in populer frontend technologies, but my major
-        focus is on ReactJs.
+        developer. I like to learn new technologies. I am interested in populer
+        frontend technologies.
         <br />
       </p>
+      <div className={styles.edu}>
+        <h3>Education</h3>
+        <b>Bachelor Of Technology In Computer Science & Engineering </b>
+        <span>
+          <br />
+          <br />
+          <i>
+            Kashi Institute Of Technology, Varanasi <br />
+            (2019-23)
+          </i>
+        </span>
+      </div>
     </div>
   );
 }
